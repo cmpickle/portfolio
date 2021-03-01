45fe7a3a441c95167cd9d10e48d53c0820818f49
Cameron Pickle <cmpickle@gmail.com>
2021-03-01 07:32:22 -0700

    Add tech flairs and about me to site

--- src/App.js
+++ src/App.js
@@ -8,44 +8,71 @@ import profile from './assets/images/profile.jpg';
 
 export const AppComponent = (props) => {
   const { classes } = props;
-  const bannerContent = () => (<React.Fragment>
-    <img src={profile} className={classes.profileImage} alt='Profile' />
-    <Typography variant="subtitle1">Hello, I'm</Typography>
-    <Typography variant="h1">Cameron Pickle</Typography>
-</React.Fragment>);
+  const bannerContent = () => (
+    <React.Fragment>
+      <img src={profile} className={classes.profileImage} alt='Profile' />
+      <Typography variant='subtitle1'>Hello, I'm</Typography>
+      <Typography variant='h1'>Cameron Pickle</Typography>
+      <header className={classes.appHeader}>
+        <Company
+          key={100}
+          company={{
+            id: 100,
+            label: 'About Me',
+            description: '',
+            flairs: [
+              { path: 'assets/images/tech/github.png', alt: 'C#' },
+              {
+                path: 'assets/images/tech/stack-overflow.png',
+                alt: 'SQL Server',
+              },
+              { path: 'assets/images/tech/linkedin.jpg', alt: 'Selenium' },
+            ],
+          }}
+        >
+          <img
+            src='https://stackoverflow.com/users/flair/1758235.png?theme=dark'
+            width='208'
+            height='58'
+            alt='profile for cmpickle at Stack Overflow, Q&amp;A for professional and enthusiast programmers'
+            title='profile for cmpickle at Stack Overflow, Q&amp;A for professional and enthusiast programmers'
+            style={{
+              border: '5px solid rgb(0, 136, 255)',
+              borderRadius: '5px',
+              margin: '40px',
+            }}
+          ></img>
+        </Company>
+        {companies.companies.map((c) => (
+          <Company key={c.id} company={c} />
+        ))}
+      </header>
+    </React.Fragment>
+  );
   return (
     <div className={classes.app}>
-      <Banner component={bannerContent}>
-      </Banner>
-      <header className={classes.appHeader}>
-
-        <div>
-          {companies.companies.map(c =>
-            <Company key={c.id} company={c} />)}
-        </div>
-      </header>
+      <Banner component={bannerContent}></Banner>
     </div>
   );
-}
+};
 
 const styles = {
   appHeader: {
-    backgroundColor: '#222',
     minHeight: '100vh',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     fontize: 'calc(10px + 2vmin)',
-    color: 'white'
+    color: 'white',
   },
   app: {
-    textAlign: 'center'
+    textAlign: 'center',
   },
   profileImage: {
     marginTop: '30vh',
     borderRadius: '50%',
-    border: '4px solid #09f'
-  }
-}
+    border: '4px solid #09f',
+  },
+};
 
 export const App = withStyles(styles)(AppComponent);
--- src/company.component.js
+++ src/company.component.js
@@ -3,49 +3,82 @@ import { Typography, Paper } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 const CompanyComponent = (props) => {
-  const {
-    company,
-    classes,
-  } = props;
+  const { company, classes, children } = props;
   return (
     <Paper className={classes.card}>
       <div className={classes.imageBackground}>
-        <img src={company.image} alt={company.name} className={classes.image} />
+        {company.image ? (
+          <img
+            src={company.image}
+            alt={company.name}
+            className={classes.image}
+          />
+        ) : (
+          <Typography variant='h2' className={classes.label}>
+            {company.label}
+          </Typography>
+        )}
       </div>
       <div className={classes.textArea}>
-        <Typography variant="h4" className={classes.content}>{company.name}</Typography>
-        <Typography className={`${classes.content} ${classes.roleStyle}`}>{company.position} {company.timeline}</Typography>
-        <Typography className={classes.content}>{company.technology}</Typography>
-        <Typography className={classes.content}>{company.description}</Typography>
+        <Typography variant='h4' className={classes.content}>
+          {company.name}
+        </Typography>
+        <Typography className={`${classes.content} ${classes.roleStyle}`}>
+          {company.position} {company.timeline}
+        </Typography>
+        <Typography className={classes.content}>
+          {company.technology}
+        </Typography>
+        <Typography className={classes.content}>
+          {company.description}
+        </Typography>
+        {company.flairs &&
+          company.flairs.map((flair) => (
+            <img className={classes.flair} src={flair.path} alt={flair.alt} />
+          ))}
       </div>
+      {children}
     </Paper>
   );
 };
 
 const styles = {
-    image: {
-        width: '400px',
-        height: '200px',
-        objectFit: 'contain',
-        backgroundColor: '#FFFFFF'
-    },
-    content: {
-        whiteSpace: 'pre-wrap',
-        textAlign: 'initial'
-    },
-    roleStyle: {
-      color: '#09f'
-    },
-    card: {
-        margin: '20px',
-        maxWidth: '1000px'
-    },
-    textArea: {
-      padding: '20px'
-    },
-    imageBackground: {
-      backgroundColor: 'white'
-    }
-}
+  label: {
+    color: '#000000',
+    padding: '60px 200px',
+  },
+  image: {
+    width: '400px',
+    height: '200px',
+    objectFit: 'contain',
+    backgroundColor: '#FFFFFF',
+  },
+  content: {
+    whiteSpace: 'pre-wrap',
+    textAlign: 'initial',
+  },
+  roleStyle: {
+    color: '#09f',
+  },
+  card: {
+    margin: '20px',
+    maxWidth: '1000px',
+  },
+  textArea: {
+    padding: '20px',
+  },
+  imageBackground: {
+    backgroundColor: 'white',
+  },
+  flair: {
+    width: '100px',
+    height: '100px',
+    margin: '20px',
+    border: '5px solid #08F',
+    objectFit: 'contain',
+    borderRadius: '15%',
+    backgroundColor: '#FFF',
+  },
+};
 
 export const Company = withStyles(styles)(CompanyComponent);
