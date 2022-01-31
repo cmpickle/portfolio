f1c38a09f9d0435da488b86b1bbde0285676a837
Cameron Pickle <cmpickle@gmail.com>
2022-01-31 07:28:55 -0700

    Updates

---
+++ precache-manifest.c780caed0c139884fe1894074b8264be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ecb249b05be17ea04f9",
+    "url": "/static/css/main.53fefa04.chunk.css"
+  },
+  {
+    "revision": "8ecb249b05be17ea04f9",
+    "url": "/static/js/main.50425f31.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "dd0c77d8b2965395c410",
+    "url": "/static/js/2.798e39df.chunk.js"
+  },
+  {
+    "revision": "7c00b37f75d5848df88340aa52096e85",
+    "url": "/static/media/profile.7c00b37f.jpg"
+  },
+  {
+    "revision": "778e886fe19d64c223d9de482aceee56",
+    "url": "/static/media/olympus.778e886f.jpg"
+  },
+  {
+    "revision": "465861dd85de63ff858dbcce001a4b80",
+    "url": "/static/media/3DPrinting.465861dd.svg"
+  },
+  {
+    "revision": "c3e0e41dbdb8dd4d6bc1446047b2ac7f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60055db85dd44dfdc4bd9a6f307ebf58.js"
+  "/precache-manifest.c780caed0c139884fe1894074b8264be.js"
 );
 
 workbox.clientsClaim();
